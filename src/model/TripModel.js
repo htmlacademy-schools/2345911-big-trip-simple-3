5a295d610d36d585c95ce9ab8ddc30c5eb388c9d
HQlog <90633620+HQlog@users.noreply.github.com>
2023-06-11 22:13:07 +0300

    a

--- src/model/TripModel.js
+++ src/model/TripModel.js
@@ -1,7 +1,7 @@
 import { generateTripEvents } from '../mock/trip-event';
 
 export default class TripEventsModel {
-  tripEvents = generateTripEvents(2);
+  tripEvents = generateTripEvents(0);
 
   getTripEvents = () => this.tripEvents;
 }
--- src/presenter/TripPresenter.js
+++ src/presenter/TripPresenter.js
@@ -4,22 +4,17 @@ import TripEventsSortingView from '../view/TripEventsSortingView';
 import TripEventView from '../view/TripEventView';
 
 class TripPresenter {
-  tripListComponent = new TripEventsListView();
-
   init(container, tripModel) {
     this.container = container;
     this.tripModel = tripModel;
     this.tripEventsData = tripModel.getTripEvents();
-    console.log('Trip Events: ', this.tripEventsData); // Debug information. Will delete soon
+    console.log('Trip Events: ', this.tripEventsData); // Debug information. Will be deleted soon
+
+    const tripEvents = this.tripEventsData.map((tripData) => new TripEventView(tripData));
+    this.tripListComponent = new TripEventsListView(tripEvents);
 
     render(new TripEventsSortingView(), this.container);
     render(this.tripListComponent, this.container);
-
-    for (let i = 0; i < this.tripEventsData.length; i++) {
-      const tripData = this.tripEventsData[i];
-      const tripEvent = new TripEventView(tripData);
-      this.tripListComponent.addComponent(tripEvent);
-    }
   }
 }
 
