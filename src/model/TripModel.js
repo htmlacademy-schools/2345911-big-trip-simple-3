f0c3fc63e1a86aea06f5fa74479b807fb4ae824a
HQlog <90633620+HQlog@users.noreply.github.com>
2023-06-11 22:19:10 +0300

    a

--- src/model/TripModel.js
+++ src/model/TripModel.js
@@ -1,7 +1,9 @@
 import { generateTripEvents } from '../mock/trip-event';
 
 export default class TripEventsModel {
-  tripEvents = generateTripEvents(2);
+  #tripEvents = generateTripEvents(2);
 
-  getTripEvents = () => this.tripEvents;
+  get tripEvents() {
+    return this.#tripEvents;
+  }
 }
